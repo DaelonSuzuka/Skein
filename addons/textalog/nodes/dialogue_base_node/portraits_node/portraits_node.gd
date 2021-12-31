b90c2448999c3ef19332a67cc1acd4cf4a7fce48
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-12-31 02:30:06 -0500

    Added textalog plugin for additional reference

---
+++ addons/textalog/nodes/dialogue_base_node/portraits_node/portraits_node.gd
@@ -0,0 +1,198 @@
+tool
+extends Control
+class_name PortraitManager
+
+##
+## Base class for any DialogPortraitManager node.
+##
+## @desc:
+##     Manages the displayed portrait of any character, saving their reference.
+##
+## @tutorial(Online Documentation): https://anidemdex.gitbook.io/godot-dialog-plugin/documentation/node-class/class_dialog-portrait-manager
+##
+
+## Emmited when a character portrait was added.
+signal portrait_added(character, new_portrait)
+
+## Emmited when a [Class Portrait] was changed with a new one.
+signal portrait_changed(character, new_portrait)
+
+## Emmited when a character portrait was removed from scene.
+signal portrait_removed(character)
+
+export(Vector2) var preview_relative_position := Vector2(0.35,0.2) setget _set_preview_relative_position
+export(Vector2) var preview_relative_size := Vector2(0.3, 0.7) setget _set_preview_relative_size
+
+var reference_rect:ReferenceRect
+
+# {CharacterResource: PortraitNode(TextureRect)}
+var portraits:Dictionary = {}
+
+## Adds a portrait for character to the scene
+func add_portrait(
+	character:Character,
+	portrait:Portrait,
+	rect_data:Dictionary = {},
+	texture_data:Dictionary = {}
+	) -> void:
+	
+	if (not character) or (not portrait):
+		emit_signal("portrait_added", character, portrait)
+		return
+	
+	# Remove previous node
+	if character in portraits:
+		remove_portrait(character)
+		emit_signal("portrait_changed", character, portrait)
+	
+	var _texture_rect:TextureRect = TextureRect.new()
+	connect("tree_exiting", _texture_rect, "queue_free")
+	
+	if character.display_name:
+		_texture_rect.name = character.display_name
+	
+	portraits[character] = _texture_rect
+	
+	# Focus and _input
+	_texture_rect.mouse_filter = MOUSE_FILTER_IGNORE
+	_texture_rect.focus_mode = Control.FOCUS_NONE
+	
+	add_child(_texture_rect)
+	
+	# I know that I can iterate over property list to copy property values
+	# but i want to keep the control over this section here
+	
+	# Node configuration to resize according the screen
+	_texture_rect.anchor_left = 0
+	_texture_rect.anchor_top = 0
+	_texture_rect.anchor_right = 1
+	_texture_rect.anchor_bottom = 1
+	
+	
+	# Size
+	var ignore_ref_size:bool = rect_data.get("ignore_reference_size", false)
+	var ref_size:Vector2 = reference_rect.rect_size
+	if ignore_ref_size:
+		var _rel_size:Vector2 = rect_data.get("size", Vector2(0.3, 0.7))
+		ref_size = _get_relative_position(_rel_size)
+		ref_size.x = max(ref_size.x, 0)
+		ref_size.y = max(ref_size.y, 0)
+	_texture_rect.rect_size = ref_size
+	
+	# Position
+	var ignore_ref_pos:bool = rect_data.get("ignore_reference_position", false)
+	var ref_position:Vector2 = reference_rect.rect_position
+	if ignore_ref_pos:
+		var _rel_pos:Vector2 = rect_data.get("position", Vector2(0.35,0.2))
+		ref_position = _get_relative_position(_rel_pos)
+	_texture_rect.rect_position = ref_position
+	_texture_rect.rect_pivot_offset = _texture_rect.rect_size/2
+	
+	# Rotation
+	var ignore_ref_rot:bool = rect_data.get("ignore_reference_rotation", false)
+	var ref_rotation = reference_rect.rect_rotation
+	if ignore_ref_rot:
+		ref_rotation = rect_data.get("rotation", 0.0)
+	_texture_rect.rect_rotation = ref_rotation
+	
+	# TextureRect
+	_texture_rect.expand = texture_data.get("expand", true)
+	_texture_rect.stretch_mode = texture_data.get("stretch_mode", TextureRect.STRETCH_KEEP_ASPECT_CENTERED)
+	_texture_rect.flip_h = texture_data.get("flip_h", false)
+	_texture_rect.flip_v = texture_data.get("flip_v", false)
+	
+	_texture_rect.texture = portrait.image
+	
+	grab_portrait_focus(_texture_rect)
+	
+	emit_signal("portrait_added", character, portrait)
+
+
+func remove_portrait(character:Character) -> void:
+	if character:
+		var _old_p = portraits.get(character, null)
+		if _old_p != null:
+			_old_p.queue_free()
+		portraits.erase(character)
+	
+	emit_signal("portrait_removed", character)
+
+
+func remove_all_portraits() -> void:
+	for character in portraits.keys():
+		remove_portrait(character)
+
+
+func change_portrait(character:Character, portrait:Portrait) -> void:
+	if not character or not portrait:
+		emit_signal("portrait_changed", character, portrait)
+		return
+	
+	if not character in portraits:
+		add_portrait(character, portrait)
+		return
+	
+	portraits[character].texture = portrait.image
+	grab_portrait_focus(portraits[character])
+	emit_signal("portrait_changed", character, portrait)
+
+
+func grab_portrait_focus(char_portrait_node:TextureRect) -> void:
+	char_portrait_node.raise()
+
+
+##########
+# Private things
+##########
+
+func _get_relative_position(from:Vector2) -> Vector2:
+	var _position:Vector2 = Vector2()
+	_position.x = float(lerp(0, rect_size.x, from.x))
+	_position.y = float(lerp(0, rect_size.y, from.y))
+	return _position
+
+
+func _set_preview_relative_position(value:Vector2) -> void:
+	preview_relative_position = value
+	
+	if not is_instance_valid(reference_rect):
+		return
+	
+	var _relative_position := _get_relative_position(preview_relative_position)	
+	reference_rect.rect_position = _relative_position
+
+
+func _set_preview_relative_size(value:Vector2) -> void:
+	preview_relative_size = value
+	
+	if not is_instance_valid(reference_rect):
+		return
+	
+	var _relative_size := _get_relative_position(preview_relative_size)
+	reference_rect.rect_size = _relative_size
+
+
+func _init() -> void:
+	reference_rect = ReferenceRect.new()
+	reference_rect.anchor_left = 0
+	reference_rect.anchor_top = 0
+	reference_rect.anchor_right = 1
+	reference_rect.anchor_bottom = 1
+	reference_rect.grow_horizontal = Control.GROW_DIRECTION_BOTH
+	reference_rect.grow_vertical = Control.GROW_DIRECTION_BOTH
+	add_child(reference_rect)
+
+
+func _notification(what) -> void:
+	match what:
+		NOTIFICATION_PREDELETE:
+			if is_instance_valid(reference_rect):
+				reference_rect.queue_free()
+		NOTIFICATION_RESIZED:
+#			var _relative_size := _get_relative_position(preview_relative_size)
+#			var _relative_position := _get_relative_position(preview_relative_position)
+#			printt(preview_relative_position, preview_relative_size, _relative_position, _relative_size, rect_size)
+#			reference_rect.rect_size = _relative_size
+#			reference_rect.rect_position = _relative_position
+			set("preview_relative_position", preview_relative_position)
+			set("preview_relative_size", preview_relative_size)
---
+++ addons/textalog/resources/character_class/character_class.gd
@@ -0,0 +1,140 @@
+tool
+extends Resource
+class_name Character, "res://addons/textalog/assets/icons/character_icon.png"
+
+##
+## Base class for all characters resource.
+##
+## @desc: 
+##     Character resource acts as a data container
+##     to all of your characters and related portraits to that character.
+##
+## @tutorial(Online Documentation): https://anidemdex.gitbook.io/godot-dialog-plugin/documentation/resource-class/class_dialog-character-resource
+##
+
+## Name of the character.
+var name:String = "" setget set_name
+
+## Name of the character that'll be displayed in game. By default, is the same as name
+var display_name:String = "" setget set_display_name,get_display_name
+var _display_name:String = "" setget set_display_name
+
+## Color of the character name
+export(Color) var color:Color = Color.white setget set_color
+
+## Character icon. Used by the editor.
+export(Texture) var icon:Texture setget set_icon
+
+## Character sounds when talking.
+var blip_sounds:Array = [] setget _set_blip_sounds
+
+## Collection of portraits that'll be displayed in game
+var portraits:Array = [] setget _set_portraits
+
+func _init() -> void:
+	portraits = []
+	blip_sounds = []
+
+
+## Adds a [DialogPortraitResource] in [member portraits]
+func add_portrait(portrait) -> void:
+	if not portrait in portraits:
+		portraits.append(portrait)
+		emit_changed()
+
+
+## Adds a [AudioStream] in [member blip_sounds]
+func add_blip_sound(blip_sound:AudioStream) -> void:
+	if not blip_sound in blip_sounds:
+		blip_sounds.append(blip_sound)
+		emit_changed()
+
+
+## Removes a [DialogPortraitResource] from [member portraits]
+func remove_portrait(portrait) -> void:
+	if portrait in portraits:
+		portraits.erase(portrait)
+		emit_changed()
+
+
+
+## Removes a [AudioStream] from [member blip_sounds]
+func remove_blip_sound(blip_sound:AudioStream) -> void:
+	if blip_sound in blip_sounds:
+		blip_sounds.erase(blip_sound)
+		emit_changed()
+
+
+func set_name(value:String) -> void:
+	name = value
+	resource_name = value
+	property_list_changed_notify()
+	emit_changed()
+
+
+func set_display_name(value:String) -> void:
+	display_name = value
+	emit_changed()
+	property_list_changed_notify()
+
+
+func set_color(value:Color) -> void:
+	color = value
+	emit_changed()
+
+
+func set_icon(value:Texture) -> void:
+	icon = value
+	emit_changed()
+
+func _set_blip_sounds(value:Array) -> void:
+	blip_sounds = value.duplicate()
+	emit_changed()
+	property_list_changed_notify()
+
+
+func get_display_name() -> String:
+	if display_name:
+		return display_name
+	else:
+		return name
+
+
+func _get_property_list() -> Array:
+	var p:Array = []
+	var default_usage := PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE
+	p.append({"type":TYPE_STRING, "name":"name", "usage":default_usage, "hint":PROPERTY_HINT_PLACEHOLDER_TEXT, "hint_string":name})
+	p.append({"type":TYPE_STRING, "name":"_display_name", "usage":default_usage, "hint":PROPERTY_HINT_PLACEHOLDER_TEXT, "hint_string":name})
+	p.append({"type":TYPE_ARRAY, "name":"blip_sounds", "usage":default_usage, "hint":24, "hint_string":"17/17:AudioStream"})
+	
+	p.append({"type":TYPE_NIL, "name":"Portraits", "usage":PROPERTY_USAGE_CATEGORY})
+	p.append({"type":TYPE_ARRAY,"name":"portraits","usage":default_usage, "hint":24, "hint_string":"17/17:Resource"})
+	return p
+
+
+func _set_portraits(value:Array) -> void:
+	portraits = value.duplicate()
+	emit_changed()
+	property_list_changed_notify()
+
+
+func _get(property: String):
+	if property == "portraits_number":
+		return portraits.size()
+
+
+func _hide_script_from_inspector():
+	return true
+
+
+func property_can_revert(property:String) -> bool:
+	var _r_p = ["display_name", "blip_sounds", "portraits"]
+	return property in _r_p
+
+
+func property_get_revert(property:String):
+	match property:
+		"display_name":
+			return ""
+		"blip_sounds", "portraits":
+			return []
