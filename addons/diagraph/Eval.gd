ac08af23b282c5a889c0e0ec1c8faa11120b1178
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-01-18 02:10:07 -0500

    Replaced EvalBuilder with Eval

---
+++ addons/diagraph/Eval.gd
@@ -0,0 +1,19 @@
+extends Node
+
+# ******************************************************************************
+
+static func evaluate(input:String, global:Object=null, locals:Dictionary={}, _show_error:bool=true):
+	var _evaluated_value = null
+	var _expression = Expression.new()
+	
+	var _err = _expression.parse(input, PoolStringArray(locals.keys()))
+	
+	if _err != OK:
+		push_warning(_expression.get_error_text())
+	else:
+		_evaluated_value = _expression.execute(locals.values(), global, _show_error)
+		
+		if _expression.has_execute_failed():
+			return input
+		
+	return _evaluated_value
