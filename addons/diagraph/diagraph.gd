483722254897367a7257696c3ab2f68471f03fd9
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-01-18 02:07:38 -0500

    Removed diagraph main screen

--- addons/diagraph/diagraph.gd
+++ addons/diagraph/diagraph.gd
@@ -3,29 +3,20 @@ extends EditorPlugin
 
 # ******************************************************************************
 
-const editor_class = preload("res://addons/diagraph/DiagraphEditor.tscn")
+const editor_class = preload("res://addons/diagraph/editor/DiagraphEditor.tscn")
 var editor_instance
+var editor_button
 
 # ******************************************************************************
 
 func _enter_tree():
 	editor_instance = editor_class.instance()
-	get_editor_interface().get_editor_viewport().add_child(editor_instance)
-	editor_instance.hide()
+	editor_button = add_control_to_bottom_panel(editor_instance, 'Diagraph')
 
 func _exit_tree():
 	if editor_instance:
+		remove_control_from_bottom_panel(editor_instance)
 		editor_instance.queue_free()
-	
-func get_plugin_name():
-	return "Diagraph"
-	
-func make_visible(visible):
-	editor_instance.visible = visible
-
-func has_main_screen():
-	return true
-	
 
 func get_plugin_icon():
-	return preload("diagraph_icon.png")	
+	return preload("resources/diagraph_icon.png")	
