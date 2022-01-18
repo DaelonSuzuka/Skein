e02e55c1815979dfcf18e627b814ac620558756f
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-01-18 03:34:52 -0500

    Rearranged DialogBox and removed Options script

--- addons/diagraph/dialog_box/DialogBox.gd
+++ addons/diagraph/dialog_box/DialogBox.gd
@@ -3,17 +3,20 @@ extends Control
 
 # ******************************************************************************
 
+var OptionButton = preload('res://addons/diagraph/dialog_box/OptionButton.tscn')
+
 signal done
 
-var waiting_for_choice = false
+var waiting_for_choice := false
+var active := false
 
 # ******************************************************************************
 
 func _ready():
-	$Options.connect('option_selected', self, 'pressed')
+	connect('option_selected', self, 'pressed')
 
 func _input(event):
-	if !visible or waiting_for_choice:
+	if !visible or !active or waiting_for_choice:
 		return
 	if event is InputEventKey and event.pressed:
 		if event.as_text() == 'Enter':
@@ -30,7 +33,26 @@ var characters = {
 	'Pico': load('res://characters/Pico/Pico.tscn').instance(),
 }
 
+# ******************************************************************************
+
+func add_option(option, value=null):
+	var button = OptionButton.instance()
+
+	var arg = value if value else option
+	button.connect("pressed", self, "option_selected", [arg])
+	button.text = option
+
+	$Options.add_child(button)
+
+func remove_options() -> void:
+	for child in $Options.get_children():
+		if child is Button:
+			child.queue_free()
+
+# ******************************************************************************
+
 func start(new_nodes, first):
+	active = true
 	$Name/Outline.modulate = Color.white
 	$TextBox/Outline.modulate = Color.white
 	nodes = new_nodes
@@ -43,13 +65,14 @@ func next():
 	if current_line == current_data.text.size():
 		if current_data.next == 'none':
 			hide()
+			active = false
 			emit_signal('done')
 			return
 		if current_data.next == 'choice':
 			waiting_for_choice = true
 			for c in current_data.choices:
 				if current_data.choices[c].choice:
-					$Options.add_option(current_data.choices[c].choice, c)
+					add_option(current_data.choices[c].choice, c)
 			if $Options.get_child_count():
 				$Options.get_child(0).grab_focus()
 			return
@@ -83,7 +106,8 @@ func next():
 
 	current_line += 1
 
-func pressed(choice):
+func option_selected(choice):
+	remove_options()
 	waiting_for_choice = false
 	current_node = current_data.choices[choice].next
 	current_line = 0
