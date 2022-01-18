1990e261cd950c1d208416364c8b933ffe0ac689
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-01-18 13:06:26 -0500

    Moved TextBox script into DialogBox

--- addons/diagraph/dialog_box/DialogBox.gd
+++ addons/diagraph/dialog_box/DialogBox.gd
@@ -4,8 +4,18 @@ extends Control
 # ******************************************************************************
 
 var OptionButton = preload('res://addons/diagraph/dialog_box/OptionButton.tscn')
+var Eval = preload('res://addons/diagraph/utils/Eval.gd').new()
 
 signal done
+
+var TextTimer := Timer.new()
+var original_cooldown := 0.05
+var next_char_cooldown := original_cooldown
+
+signal line_finished
+signal character_added(c)
+
+onready var text_box = $TextBox
 
 var waiting_for_choice := false
 var active := false
@@ -13,7 +23,9 @@ var active := false
 # ******************************************************************************
 
 func _ready():
-	pass
+	add_child(TextTimer)
+	TextTimer.connect('timeout', self, 'process_text')
+	TextTimer.one_shot = true
 
 func _input(event):
 	if !visible or !active or waiting_for_choice:
@@ -102,7 +114,7 @@ func next():
 	$Name/Outline.modulate = color
 	$TextBox/Outline.modulate = color
 	$Name.text = name
-	$TextBox.set_line(line)
+	set_line(line)
 
 	current_line += 1
 
@@ -113,3 +125,85 @@ func option_selected(choice):
 	current_line = 0
 	current_data = nodes[current_node].parse()
 	next()
+
+# ******************************************************************************
+
+
+var next_line := ''
+var line_index := 0
+
+func set_line(line):
+	next_line = line
+	line_index = 0
+	text_box.bbcode_text = ''
+	$DebugLog.text = ''
+	next_char_cooldown = original_cooldown
+	TextTimer.start(next_char_cooldown)
+
+func speed(value=original_cooldown):
+	next_char_cooldown = value
+
+func process_text():
+	if line_index == next_line.length():
+		emit_signal('line_finished')
+		TextTimer.stop()
+		return
+
+	var next_char = next_line[line_index]
+	var cooldown = next_char_cooldown
+
+	match next_char:
+		'{': # detect commands
+			if next_line[line_index + 1] == '{':
+				var end = next_line.findn('}}', line_index)
+				if end != -1:
+					var command = next_line.substr(line_index, end - line_index + 2)
+					line_index = end + 2
+					$DebugLog.text += '\nexpansion: ' + command
+			else:
+				var end = next_line.findn('}', line_index)
+				if end != -1:
+					var command = next_line.substr(line_index, end - line_index + 1)
+					line_index = end + 1
+					var result = Eval.evaluate(command.lstrip('{').rstrip('}'), self)
+					$DebugLog.text += '\ncommand: ' + command
+					process_text()
+		'<': # reserved for future use
+			var end = next_line.findn('>', line_index)
+			if end != -1:
+				var block = next_line.substr(line_index, end - line_index + 1)
+				$DebugLog.text += '\nangle_brackets: ' + block
+				line_index = end + 1
+		'[': # detect chunks of bbcode
+			var end = next_line.findn(']', line_index)
+			if end != -1:
+				var block = next_line.substr(line_index, end - line_index + 1)
+				$DebugLog.text += '\nbbcode: ' + block
+				text_box.bbcode_text += block
+				line_index = end + 1
+				process_text()
+		'|': # pipe denotes chunks of text that should pop all at once
+			var end = next_line.findn('|', line_index + 1)
+			if end != -1:
+				var chunk = next_line.substr(line_index + 1 , end - line_index - 1)
+				$DebugLog.text += '\npop: ' + chunk
+				text_box.bbcode_text += chunk
+				line_index = end + 1
+		'_': # pause
+			cooldown = 0.25
+			$DebugLog.text += '\npause'
+			line_index += 1
+		'\\': # escape the next character
+			$DebugLog.text += '\nescape'
+			line_index += 1
+			text_box.bbcode_text += next_line[line_index]
+			emit_signal('character_added', next_line[line_index])
+			line_index += 1
+		_: # not a special character, just print it
+			text_box.bbcode_text += next_char
+			emit_signal('character_added', next_char)
+			line_index += 1
+
+	TextTimer.start(cooldown)
+
+
