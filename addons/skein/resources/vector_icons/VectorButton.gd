646536005789dcc95cdfc964ea324df63bf1d029
DaelonSuzuka <daelonsuzuka@gmail.com>
2025-02-23 19:42:34 -0500

    Reimplement VectorButton

--- addons/skein/resources/vector_icons/VectorButton.gd
+++ addons/skein/resources/vector_icons/VectorButton.gd
@@ -1,57 +1,86 @@
-tool
+@tool
 extends Button
 
 # ******************************************************************************
 
-export(String, 
-	"AntDesign", 
-	"Entypo", 
-	"EvilIcons", 
-	"Feather", 
-	"FontAwesome",
-	"FontAwesome5", 
-	"Fontisto", 
-	"Foundation", 
-	"Ionicons", 
-	"MaterialCommunityIcons",
-	"MaterialIcons", 
-	"Octicons", 
-	"SimpleLineIcons", 
-	"Zocial")\
-	var icon_set = "Ionicons" setget _set_icon_set
+enum Fonts {
+	AntDesign,
+	Entypo,
+	EvilIcons,
+	Feather,
+	FontAwesome,
+	FontAwesome5,
+	Fontisto,
+	Foundation,
+	Ionicons,
+	MaterialCommunityIcons,
+	MaterialIcons,
+	Octicons,
+	SimpleLineIcons,
+	Zocial
+}
 
-export(int, 0, 1000, 1) var size = 16 setget _set_size
-export var icon_name = "ios-analytics" setget _set_icon
-export var filter = false setget _set_filter
+const FONT_DIR := 'res://addons/skein/resources/vector_icons/fonts/'
 
-var Cheatsheet = {}
-var _font = DynamicFont.new()
+@export var icon_set := Fonts.FontAwesome:
+	set(name):
+		icon_set = name
+		var font = load(str(FONT_DIR, Fonts.find_key(name), ".gd"))
+		if font != null:
+			font_map = font.Cheatsheet
 
-func _set_size(p_size):
-	size = p_size
-	_font.set_size(p_size)
+			if icon_name not in font_map:
+				for key in font_map:
+					icon_name = key
+					break
 
-func _set_icon(p_icon):
-	icon_name = p_icon
-	var iconcode = ""
-	if p_icon in Cheatsheet:
-		iconcode = Cheatsheet[p_icon]
-	set_text(iconcode)
+			add_theme_font_override('font', font.FontData)
+			update_content()
+			notify_property_list_changed()
 
-func _set_filter(f):
-	filter = f
-	if is_inside_tree():
-		_font.set_use_filter(f)
+@export var icon_size := 16:
+	set(p_size):
+		icon_size = p_size
+		add_theme_font_size_override('font_size', p_size)
 
-func _set_icon_set(name):
-	icon_set = name
-	var font = load(str('res://addons/skein/resources/vector_icons/fonts/', name, ".gd"))
-	if font != null:
-		self.Cheatsheet = font.Cheatsheet
-		_font = DynamicFont.new()
-		_font.set_font_data(font.FontData)
-		set("custom_fonts/font", _font)
-		update_content()
+var icon_name := "ios-analytics":
+	set(p_icon):
+		icon_name = p_icon
+		var iconcode = ""
+		if p_icon in font_map:
+			iconcode = font_map[p_icon]
+		set_text(iconcode)
+
+func _get_property_list():
+	var properties = [
+		# {
+		# 	"name": "icon_set",
+		# 	"type": TYPE_INT,
+		# 	"hint": PROPERTY_HINT_ENUM,
+		# 	"hint_string": ','.join(Fonts.keys()),
+		# },
+		# {
+		# 	"name": "icon_size",
+		# 	"type": TYPE_INT,
+		# },
+		{
+			"name": "icon_name",
+			"type": TYPE_STRING,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ','.join(font_map.keys()),
+		}
+	]
+
+	return properties
+
+# @export var filter := false:
+# 	set(f):
+# 		filter = f
+# 		if is_inside_tree():
+# 			_font.set_use_filter(f)
+
+var font_map = {}
+var _font := FontFile.new()
 
 # ******************************************************************************
 
@@ -60,6 +89,6 @@ func _ready():
 	update_content()
 	
 func update_content():
-	self.size = size
+	self.icon_size = icon_size
 	self.icon_name = icon_name
-	self.filter = filter
+	# self.filter = filter
