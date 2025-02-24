bff3ea3cc83fa3f6538d0a555c89ba85dfc197c9
David Kincaid <daelonsuzuka@gmail.com>
2025-02-23 20:34:08 -0500

    Update vector icons

--- addons/skein/resources/vector_icons/VectorButton.gd
+++ addons/skein/resources/vector_icons/VectorButton.gd
@@ -22,10 +22,10 @@ enum Fonts {
 
 const FONT_DIR := 'res://addons/skein/resources/vector_icons/fonts/'
 
-@export var icon_set := Fonts.FontAwesome:
-	set(name):
-		icon_set = name
-		var font = load(str(FONT_DIR, Fonts.find_key(name), ".gd"))
+var icon_set := Fonts.FontAwesome:
+	set(value):
+		icon_set = value
+		var font = load(str(FONT_DIR, Fonts.find_key(value), ".gd"))
 		if font != null:
 			font_map = font.Cheatsheet
 
@@ -33,61 +33,128 @@ const FONT_DIR := 'res://addons/skein/resources/vector_icons/fonts/'
 				for key in font_map:
 					icon_name = key
 					break
-
-			add_theme_font_override('font', font.FontData)
+			var _font := FontVariation.new()
+			_font.base_font = font.FontData
+			set('theme_override_fonts/font',_font)
 			update_content()
 			notify_property_list_changed()
 
-@export var icon_size := 16:
-	set(p_size):
-		icon_size = p_size
-		add_theme_font_size_override('font_size', p_size)
+var filter := true:
+	set(value):
+		filter = value
+		set('texture_filter', 0 if value else 1)
 
-var icon_name := "ios-analytics":
-	set(p_icon):
-		icon_name = p_icon
-		var iconcode = ""
-		if p_icon in font_map:
-			iconcode = font_map[p_icon]
-		set_text(iconcode)
+var icon_size := 16:
+	set(value):
+		icon_size = value
+		set('theme_override_font_sizes/font_size',value)
+
+var icon_color := Color.WHITE:
+	set(value):
+		icon_color = value
+		set('theme_override_colors/font_color',value)
+
+var outline_size := 0:
+	set(value):
+		outline_size = value
+		set('theme_override_constants/outline_size',value)
+		notify_property_list_changed()
+
+var outline_color := Color.WHITE:
+	set(value):
+		outline_color = value
+		set('theme_override_colors/font_outline_color',value)
+
+var iconcode := ''
+var icon_name := '':
+	set(value):
+		icon_name = value
+		iconcode = font_map.get(value, '')
+		if !pressed:
+			set_text(iconcode)
+
+var pressed_iconcode := ''
+var pressed_icon_name := '':
+	set(value):
+		pressed_icon_name = value
+		pressed_iconcode = font_map.get(value, '')
+		if pressed:
+			set_text(pressed_iconcode)
 
 func _get_property_list():
 	var properties = [
-		# {
-		# 	"name": "icon_set",
-		# 	"type": TYPE_INT,
-		# 	"hint": PROPERTY_HINT_ENUM,
-		# 	"hint_string": ','.join(Fonts.keys()),
-		# },
-		# {
-		# 	"name": "icon_size",
-		# 	"type": TYPE_INT,
-		# },
 		{
-			"name": "icon_name",
+			"name": "icon_set",
+			"type": TYPE_INT,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ','.join(Fonts.keys()),
+		},
+		{
+			"name": "filter",
+			"type": TYPE_BOOL,
+		},
+		{
+			"name": "toggle_mode",
+			"type": TYPE_BOOL,
+		},
+		{
+			"name": "icon_size",
+			"type": TYPE_INT,
+		},
+		{
+			"name": "icon_color",
+			"type": TYPE_COLOR,
+		},
+		{
+			"name": "outline_size",
+			"type": TYPE_INT,
+		},
+	]
+	if outline_size > 0:
+		properties.append({
+			"name": "outline_color",
+			"type": TYPE_COLOR,
+		})
+
+	properties.append({
+		"name": "icon_name",
+		"type": TYPE_STRING,
+		"hint": PROPERTY_HINT_ENUM,
+		"hint_string": ','.join(font_map.keys()),
+	})
+	if toggle_mode:
+		properties.append({
+			"name": "pressed_icon_name",
 			"type": TYPE_STRING,
 			"hint": PROPERTY_HINT_ENUM,
 			"hint_string": ','.join(font_map.keys()),
-		}
-	]
+		})
 
 	return properties
 
-# @export var filter := false:
-# 	set(f):
-# 		filter = f
-# 		if is_inside_tree():
-# 			_font.set_use_filter(f)
+func _set(property: StringName, value: Variant) -> bool:
+	if property == "toggle_mode":
+		notify_property_list_changed()
+
+	return false
 
 var font_map = {}
-var _font := FontFile.new()
+var _font := FontVariation.new()
 
 # ******************************************************************************
 
 func _ready():
 	self.icon_set = icon_set
 	update_content()
-	
+	toggled.connect(_on_toggled)
+
+func _on_toggled(on: bool):
+	print('toggled')
+	if on:
+		set_text(pressed_iconcode)
+	else:
+		set_text(iconcode)
+
 func update_content():
 	self.icon_size = icon_size
 	self.icon_name = icon_name
